847d514853711863ab698fba11e8e59c1be8d2a4
Victor Oliveira Barros do Nascimento <victoroliveirabarros@outlook.com>
2023-10-04 19:20:29 -0300

    feat: minimal application

---
+++ app/domain/vote_repository.py
@@ -0,0 +1,19 @@
+import abc
+from typing import List
+
+from app.domain.vote import Vote
+
+
+class VoteRepository(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def add(self, vote: Vote) -> Vote:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def all(self) -> List[Vote]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def total(self) -> int:
+        raise NotImplementedError
+    
---
+++ tests/domain/vote_test.py
@@ -0,0 +1,11 @@
+import uuid
+from app.domain.vote import Vote
+
+def test_vote_existing_vote_id():
+    vote_id = str(uuid.uuid4())
+    assert Vote(vote_id).vote_id == vote_id
+
+def test_vote_defaults():
+    vote_id = str(uuid.uuid4())
+    assert Vote().vote_id != vote_id
+    
